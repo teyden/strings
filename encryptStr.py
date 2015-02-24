caa1b15a59ff46a548f26271b1723eebb6835364
teyden <teydenn@gmail.com>
2015-02-23 20:19:19 -0500

    added complete version of encryptStr

--- encryptStr.py
+++ encryptStr.py
@@ -1,16 +1,25 @@
 import math
-# floor, ceil, sqrt
 
-def encryptStr(inputStr):
+def encryptStr():
 
     """
-    encrypts input string
-    """
+    encryptStr encodes input string and 
+    returns encrypted message 
 
-    # remove white spaces
-    string = inputStr.replace(" ", "")
-    # take string length
+    Determine rows and columns based on constraints:
+        1) floor(sqrt(length)) <= rows <= columns <= ceil(sqrt(length))
+        2) rows * columns >= L
+
+
+    Sample input: "if man was meant to stay on the ground god would have given us roots"
+    Sample output: "imtgdvs fearwer mayoogo anouuio ntnnlvt wttddes aohghn sseoau"
+
+    """
+    inputStr = raw_input("Input message: ")
+    delimiter = " "
+    string = inputStr.replace(delimiter, "")
     L = len(string)
+
 
     
     # minimum numRow = lowerLimit
@@ -24,10 +33,8 @@ def encryptStr(inputStr):
     numCol = int(upperLimit)
     
 
-    # does decrementing numCol produce smaller product?
+
     for x in range(numRow, numCol+1, 1):
-        print("Range between row and col: ")
-        print(range(numRow, numCol+1, 1))
         # starting point of numRow*numCol is smallest possible value
         if (numRow * numCol < L):
             # increment numRow (smaller num) to meet threshold: r*c must be >= L
@@ -35,41 +42,43 @@ def encryptStr(inputStr):
             
     if (numRow > numCol):
         print("Error: condition violation")
-    else:
-        print("Number of rows, columns: %d %d " % (numRow, numCol))
-        
+    
+
+    # Separate sentence sections into appropriate row + col
     # elements = [["ifmanwas"],["meanttos"],...,[] * number of rows]
     elements = []
-
     for row in range(numRow):
         elements.append([string[0:numCol]])
         string = string[numCol:]
 
 
-    if (numRow * numCol < L): 
-        raise ValueError, "Product must be greater than string length"
+    # Handle case where r*c > L
+    trailSpaces = (numRow*numCol)-L
+    if (trailSpaces != 0): 
+        for i in range(numCol-trailSpaces, numCol, 1):
+            # replace trailSpaces in last row with spaces
+            elements[numRow-1][0] += delimiter
 
 
-    encrypted = ""
+    # Encode message and store in encrypted
+    encrypted = []
     for col in range(numCol):
-        print(col)
         word = ""
+
         for row in range(numRow):
-            # print("Row: %d " % row)
-            # print("Letter #: %d " % k)
+            word += elements[row][0][col]
 
-            else:
-                word += elements[row][0][col]
-                k += 1
-
-        encrypted += word + " "
+        encrypted += [word.strip()]
 
 
-    print(encrypted)
+    print(delimiter.join(encrypted))
 
 
 
 
-encryptStr("if man was meant to stay on the ground god would have given us roots")
+
+
+
+encryptStr()
 
         
